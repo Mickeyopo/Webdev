3ef641537571043a49d56c012e8b793e65534bdc
Mickeyopo <Michael.opondo@strathmore.edu>
2024-07-08 06:15:49 +0300

    db upd

--- Db.sql
+++ Db.sql
@@ -1,26 +1,21 @@
---create database
-
+-- Create database
 DROP DATABASE IF EXISTS webapp;
 CREATE DATABASE IF NOT EXISTS webapp;
 USE webapp;
 
---create table
-
+-- Create table
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users(
-    userId bigint(11) NOT NULL AUTO-INCREMENT,
-     fullname varchar(50) NOT NULL DEFAULT '',
-      username varchar(50) NOT NULL DEFAULT '',
-      email varchar(50) NOT NULL DEFAULT '',
-        password varchar(60) NOT NULL DEFAULT '',
-        userType varchar(30) NOT NULL DEFAULT '',
-        userImage varchar(50) NOT NULL DEFAULT 'userImage.jpg',
-        created bigint(10) NOT NULL DEFAULT 0,
-        updated bigint(10) NOT NULL DEFAULT 0,
-        UNIQUE KEY (username),
-        UNIQUE KEY (email),
-        PRIMARY KEY (userId)
-
-
-     
+CREATE TABLE IF NOT EXISTS users (
+    userId BIGINT NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(50) NOT NULL DEFAULT '',
+    username VARCHAR(50) NOT NULL DEFAULT '',
+    email VARCHAR(50) NOT NULL DEFAULT '',
+    password VARCHAR(60) NOT NULL DEFAULT '',
+    userType VARCHAR(30) NOT NULL DEFAULT '',
+    userImage VARCHAR(50) NOT NULL DEFAULT 'userImage.jpg',
+    created BIGINT NOT NULL DEFAULT 0,
+    updated BIGINT NOT NULL DEFAULT 0,
+    UNIQUE KEY (username),
+    UNIQUE KEY (email),
+    PRIMARY KEY (userId)
 );
